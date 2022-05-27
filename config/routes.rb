9865e535406ff8b8d5dd8a19c7f7acfa00b22a6d
Guilherme Santos <agustinho_santos@hotmail.com>
2022-05-27 12:13:19 -0300

    root is done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "lists#index"
   resources :lists, only: %i[index show create new] do
     resources :bookmarks, only: %i[new create]
   end
